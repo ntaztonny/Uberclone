9e62f19dd2424672697490b1fa7578e6af9f39de
ntaztonny <atozpp@yahoo.com>
2022-01-20 22:58:10 +0100

    Intergrated google places API into the app

--- uberClone/screens/HomeScreen.js
+++ uberClone/screens/HomeScreen.js
@@ -2,9 +2,13 @@ import React from "react";
 import { StyleSheet, Text, SafeAreaView, Image, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../Components/NavOptions";
-import MapScreen from "./MapScreen";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { selectDestination, selectOrigin, setOrign } from "../Slices/navSlice";
 
 function HomeScreen() {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -14,9 +18,38 @@ function HomeScreen() {
             uri: "https://links.papareact.com/gzs",
           }}
         />
+        <GooglePlacesAutocomplete
+          placeholder="where from?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
         <NavOptions />
       </View>
-      <MapScreen />
     </SafeAreaView>
   );
 }
