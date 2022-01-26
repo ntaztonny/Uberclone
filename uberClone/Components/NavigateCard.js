bef09a16ee074eb8ab4235ec906ab9d862dcc367
ntaztonny <atozpp@yahoo.com>
2022-01-26 06:14:33 +0100

    Fixed keyboardsafetyView, added rides and directions

--- uberClone/Components/NavigateCard.js
+++ uberClone/Components/NavigateCard.js
@@ -8,6 +8,8 @@ import { useDispatch } from "react-redux";
 import { setDestination } from "../Slices/navSlice";
 import { useNavigation } from "@react-navigation/native";
 import Navfavourites from "./Navfavourites";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Icon } from "react-native-elements";
 
 const NavigateCard = () => {
   const dispatch = useDispatch();
@@ -46,6 +48,29 @@ const NavigateCard = () => {
           />
         </View>
         <Navfavourites />
+      </View>
+      <View
+        style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}
+      >
+        <TouchableOpacity
+          onPress={() => navigation.navigate("RideOptionCard")}
+          style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon name="car" type="font-awesome" color="white" size={16} />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between w-24 px-4 py-3`}
+        >
+          <Icon
+            name="fast-food-outline"
+            type="ionicon"
+            color="black"
+            size={16}
+          />
+          <Text style={tw`text-center`}>Eats</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
--- uberClone/Components/RideOptionsCard.js
+++ uberClone/Components/RideOptionsCard.js
@@ -1,11 +1,68 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { FlatList } from "react-native-gesture-handler";
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+  const data = [
+    {
+      id: "Uber-X-123",
+      title: "UberX",
+      multiplier: 1,
+      image: "https://links.papareact.com/3pn",
+    },
+    {
+      id: "Uber-XL-456",
+      title: "Uber XL",
+      multiplier: 1.2,
+      image: "https://links.papareact.com/5w8",
+    },
+    {
+      id: "Uber-LUX-789",
+      title: "Uber LUX",
+      multiplier: 1.75,
+      image: "https://links.papareact.com/7pf",
+    },
+  ];
   return (
-    <View>
-      <Text>This is the rideOptions card</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPresss={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image } }) => (
+          <TouchableOpacity
+            //onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${
+              id === selected?.id && "bg-gray-200"
+            }`}
+          >
+            <Image
+              style={{ width: 100, height: 100, resizemode: "contain" }}
+              source={{ uri: image }}
+            />
+            <View style={tw`ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>$99</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
